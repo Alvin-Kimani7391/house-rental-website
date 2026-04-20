46b26c4013aeab61ddbf3e7fba5fcbcb68585746
alvinkimani7391-svg <alvinkimani7391@gmail.com>
2026-04-20 18:30:09 +0300

    Update server

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(cors({
   credentials: true
 }));
 
-app.options('*', cors());
+app.use(cors()); // handles preflight automatically
 // ===== BODY =====
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
